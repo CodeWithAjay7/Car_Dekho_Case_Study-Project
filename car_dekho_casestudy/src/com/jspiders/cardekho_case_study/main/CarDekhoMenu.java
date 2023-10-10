1d55d6819f2597ed7e04ae8055e7e9d44d66c4fb
Ajay Vithhal Jire <145894667+CodeWithAjay7@users.noreply.github.com>
2023-10-10 22:39:42 +0530

    Add files via upload

---
+++ car_dekho_casestudy/src/com/jspiders/cardekho_case_study/main/CarDekhoMenu.java
@@ -0,0 +1,48 @@
+package com.jspiders.cardekho_case_study.main;
+
+import java.util.Scanner;
+
+import com.jspiders.cardekho_case_study.operation.CarOperations;
+
+public class CarDekhoMenu extends CarOperations {
+
+	public static void main(String[] args) {
+		CarOperations carOper= new CarOperations();
+		carOper.createList();
+		menu();
+	}
+	public static void menu() {
+		do {
+			
+		
+		System.out.println("==============MENU============");
+		System.out.println("1.Add Car Details.\n2.Search Car Details.\n3.update Car Details\n4.delete Car details\n5.Exit\n");
+	Scanner inp = new Scanner(System.in);
+	int choice=inp.nextInt();
+	CarOperations carOper= new CarOperations();
+	switch (choice) {
+	case 1:carOper.addCarDetails();
+		  break;
+	case 2:
+		carOper.searchCarDetails();
+		break;
+	case 3:
+		carOper.updateCarDetails();
+		break;
+	case 4:
+	         	carOper.deleteCarDetails();
+	       
+		break;
+	case 5:inp.close();
+		System.exit(0);
+		break;
+
+	default:
+		System.out.println("invalid choice");
+		System.out.println("try again ");
+		
+	}
+		} while (true);
+	}
+
+}
